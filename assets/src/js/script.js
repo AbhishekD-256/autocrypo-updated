0f39ccf1606c7610dc374ddd7a17375e3189cec7
Abhishek Dolai <itsabhi183@gmail.com>
2023-06-15 11:26:31 +0530

    remove slick slider

--- assets/src/js/script.js
+++ assets/src/js/script.js
@@ -57,30 +57,6 @@ priceCardBtns.forEach((btn, i) => {
 });
 
 //-------- testimonial cards slider---------//
-$(document).ready(function () {
-  $(".testimonials-wrap").slick({
-    arrows: false,
-    dots: true,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-        },
-      },
-    ],
-  });
-});
 
 // Hero-section hide
 
